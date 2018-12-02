3181d5917aba043f384b496e7da0934ce0f5b4cd
natt17 <marketing20.nat@gmail.com>
2018-12-02 16:52:14 -0500

    Correcion de la funcion decifrar

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,6 @@
 window.cipher = {
-  encode : (offset,textElement) => {
+
+ encode : (offset,textElement) => {
       let cipherText = "";
       let resultEncode = "";
         for (let i = 0; i < textElement.length; i++) {
@@ -10,8 +11,8 @@ window.cipher = {
           else if (ubicacion >= 65 && ubicacion <=90) {
             cipherText = String.fromCharCode((ubicacion - 65 + offset) % 26 + 65);
           }
-          else if (ubicacion >= 97 && ubicacion <= 122) {
-            cipherText = String.fromCharCode((ubicacion - 97 + offset) % 26 + 97);
+          else if (ubicacion >= 97 && ubicacion <= 122){
+             cipherText = String.fromCharCode((ubicacion - 97 + offset) % 26 + 97);
           }
             resultEncode += cipherText;
         }
@@ -28,13 +29,13 @@ window.cipher = {
             decipher = " ";
           }
           else if (ubicacionDes >= 65 && ubicacionDes <=90) {
-            decipher = String.fromCharCode((ubicacionDes + 65 - offsetSecondNew) % 26 + 65);
+            decipher = String.fromCharCode(90 - (90 - ubicacionDes + offsetSecondNew) % 26);
           }
           else if (ubicacionDes >= 97 && ubicacionDes <= 122) {
-            decipher = String.fromCharCode((ubicacionDes + 97 - offsetSecondNew) % 26 + 97);
+            decipher = String.fromCharCode(122 - (122 - ubicacionDes + offsetSecondNew) % 26);
           }
           resultDecode += decipher;
         }
       return resultDecode;
     }
-};
+}
